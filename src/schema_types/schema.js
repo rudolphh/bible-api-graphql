a033d103dcc2e81a5c5fe965730ff347e05be6c8
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-05-05 02:15:18 -0700

    no content-type for now

--- src/schema_types/schema.js
+++ src/schema_types/schema.js
@@ -41,7 +41,14 @@ exports.schema = buildSchema(`
     language: Language
   }
 
-
+  type Chapter { 
+    id: String
+    bibleId: String
+    number: String
+    bookId: String
+    reference: String 
+    content: String
+  }
   
   type Book { 
     id: String
@@ -50,57 +57,17 @@ exports.schema = buildSchema(`
     name: String
     nameLong: String
     chapters: [Chapter] }
-
-  type Meta { fums: String
-    fumsId: String
-    fumsJsInclude: String
-    fumsJs: String
-    fumsNoScript: String }
   
-  type Attrs { verseId: String verseOrgIds: [String ] }
-  
-  type Items { text: String type: String attrs: Attrs }
-  
-  type Content { name: String
-    type: String
-    items: [Items ]
-    attrs: Attrs }
+  type Query {
+    bibles(language: String, abbreviation: String, name: String, ids: String, 
+        includeFullDetails: Boolean): [Bible]
+    bible(id: String!): Bible
+    books(bibleId: String!, includeChapters: Boolean, includeChaptersAndSections: Boolean): [Book]
+    book(bibleId: String!, bookId: String!, includeChapters: Boolean): Book
+    chapters(bibleId: String!, bookId: String!): [Chapter]
+    chapter(bibleId: String!, chapterId: String!, includeNotes: Boolean,
+        includeTitles: Boolean, includeChapterNumbers: Boolean, includeVerseSpans: Boolean, 
+        parallels: String): Chapter
+  }
 
-    type Parallels { id: String
-        bibleId: String
-        number: String
-        bookId: String
-        reference: String
-        copyright: String
-        verseCount: Int
-        orgId: String
-        content: [Content ] }
-      
-    type Previous { id: String number: String bookId: String }
-    
-    type Next { id: String number: String bookId: String }
-    
-    type Chapter { id: String
-        bibleId: String
-        number: String
-        bookId: String
-        reference: String
-        copyright: String
-        verseCount: Int
-        parallels: [Parallels ]
-        previous: Previous
-        next: Next
-        content: [Content ] }
-
-    type Query {
-        bibles(language: String, abbreviation: String, name: String, ids: String, 
-            includeFullDetails: Boolean): [Bible]
-        bible(id: String!): Bible
-        books(bibleId: String!, includeChapters: Boolean, includeChaptersAndSections: Boolean): [Book]
-        book(bibleId: String!, bookId: String!, includeChapters: Boolean): Book
-        chapters(bibleId: String!, bookId: String!): [Chapter]
-        chapter(bibleId: String!, chapterId: String!, contentType: String, includeNotes: Boolean,
-            includeTitles: Boolean, includeChapterNumbers: Boolean, includeVerseSpans: Boolean, 
-            parallels: String): Chapter
-        }
 `);
